1d3033404dc4549ea4faabf722a09f8f6fdb3c86
Martin Ebnoether <venty@peyote.graben>
2025-10-11 11:21:26 +0200

    Changed Player 1 and 2 key def to match left/right of player characters.

--- __main__.py
+++ __main__.py
@@ -50,14 +50,14 @@ DIR_UP = 1
 DIR_DOWN = 0
 FIRE = 5
 
-PLAYER_1_KEYS = {DIR_LEFT: pygame.K_LEFT,
+PLAYER_2_KEYS = {DIR_LEFT: pygame.K_LEFT,
                  DIR_RIGHT: pygame.K_RIGHT,
                  DIR_UP: pygame.K_UP,
                  DIR_DOWN: pygame.K_DOWN,
                  FIRE: pygame.K_RCTRL,
                  }
 
-PLAYER_2_KEYS = {DIR_LEFT: pygame.K_a,
+PLAYER_1_KEYS = {DIR_LEFT: pygame.K_a,
                  DIR_RIGHT: pygame.K_d,
                  DIR_UP: pygame.K_w,
                  DIR_DOWN: pygame.K_s,
