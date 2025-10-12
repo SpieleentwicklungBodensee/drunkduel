bcfb31fcaac4d86ea44e79c0bf938f1eb986c629
Martin Ebnoether <venty@peyote.graben>
2025-10-12 12:55:02 +0200

    Game Over Guitar sound fixed to only play once

--- game_over_screen.py
+++ game_over_screen.py
@@ -13,7 +13,8 @@ class GameOverScreen(Screen):
 
         # won't be displayed, only for voice
         self.speechMessage = Message(0, 0, ['', '', 'spieler', ['eins', 'zwei'][game_state.gameScreen.winner], 'wins'], (0, 0, 0), 0)
-
+        SFX_GAME_OVER_GUITAR.play()
+        
     def draw(self):
         ledwall.centerText('GAME OVER', y=4, color=(255, 0, 0), fontsize=2, align=False)
 
@@ -21,7 +22,7 @@ class GameOverScreen(Screen):
             if game_state.tick > 128 or game_state.tick % 32 < 16:
                 ledwall.centerText(f'PLAYER {game_state.gameScreen.winner + 1}', y=7, color=(0, 255, 0), fontsize=2, align=False)
                 ledwall.centerText('WINS!', color=(0, 255, 0), fontsize=2, align=False)
-                SFX_GAME_OVER_GUITAR.play(loops=0)
+
 
             ledwall.centerText(f'FINAL SCORE:', y=22, color=(255, 255, 255), align=False)
             ledwall.centerText(f'P1: {game_state.gameScreen.players[0].score}  P2: {game_state.gameScreen.players[1].score}', color=(255, 255, 255), align=False)
